500172370200b2044fd8081198cafc95e4a8fb75
Samuel <101996753+samussd@users.noreply.github.com>
2023-08-23 03:11:36 -0300

    improve style, add form validation and dialog box logic, add logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,10 @@
-/*TODO: 
-validate form input
-styling
-change delete icon to trashcan
-pressed button when read, unpressed when not, maybe add check
-
-EXTRA:
-sort by insertion date, title, author, year (copy with splice, then sort)
-double click to delete
-edit book
-*/
-
 const bookContainer = document.querySelector('.book-container');
 const addBtn = document.querySelector('.utility__add-btn');
 const dialog = document.querySelector('.dialog');
 const dialogForm = document.querySelector('.dialog__form');
 const submitBtn = document.querySelector('.dialog__submit-btn');
 
-const library = []
+const library = [];
 
 function Book(title, author, year, pages, read) {
   this.author = author;
@@ -61,21 +49,21 @@ function addBookToContainer(book) {
 
   const yearElem = document.createElement('p');
   yearElem.classList.add('book__year');
-  yearElem.textContent = `published ${year}`;
+  yearElem.textContent = year==='' ? '' : `published on ${year}`;
 
   const pagesElem = document.createElement('p');
   pagesElem.classList.add('book__pages');
-  pagesElem.textContent = `${pages} pages`;
+  pagesElem.textContent = pages==='' ? '' : `${pages} pages`;
 
   const readButton = document.createElement('button');
   readButton.classList.add('book__read-btn');
   readButton.setAttribute('type', 'button');
   readButton.textContent = isRead ? 'READ' : 'NOT READ';
+  if (isRead) bookElement.classList.add('book--read');
 
   const removeButton = document.createElement('button');
   removeButton.classList.add('book__remove-btn');
   removeButton.setAttribute('type', 'button');
-  removeButton.textContent = 'X';
 
   bookElement.appendChild(titleElement);
   bookElement.appendChild(authorElement);
@@ -89,26 +77,49 @@ function addBookToContainer(book) {
   readButton.addEventListener("click", (e) => {
     const btn = e.target;
     const book = e.target.parentNode;
-    bookObj = library.find(b => 
+    bookObj = library.find(b =>
       b.title === book.dataset.title &&
       b.author === book.dataset.author);
 
     bookObj.toggleReadStatus();
+    book.classList.toggle("book--read");
     btn.textContent = bookObj.read ? 'READ' : 'NOT READ';
-  })
+  });
 
   removeButton.addEventListener("click", (e) => {
     const bookElement = e.target.parentNode;
     removeBook(bookElement);
-  })
+  });
 }
 
 function removeBook(book) {
-  const bookInd = library.findIndex(b => 
+  const bookInd = library.findIndex(b =>
     b.title === book.dataset.title &&
     b.author === book.dataset.author);
   library.splice(bookInd, 1);
   book.remove();
+}
+
+function validateForm() {
+  const bookNameInput = document.getElementById('book-name-input');
+  const authorInput = document.getElementById('author-input');
+
+  const bookName = bookNameInput.value.trim();
+  const author = authorInput.value.trim();
+  
+  if (bookName !== '') bookNameInput.validity.valid = true;
+  else {
+    bookNameInput.validity.valid = false;
+    return false;
+  }
+
+  if (author !== '') authorInput.validity.valid = true;
+  else {
+    authorInput.validity.valid = false;
+    return false;
+  }
+
+  return true;
 }
 
 addBtn.addEventListener("click", () => {
@@ -117,22 +128,30 @@ addBtn.addEventListener("click", () => {
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  dialog.close();
-});
+  if (!validateForm()) return;
+  const bookNameInput = document.getElementById('book-name-input');
+  const authorInput = document.getElementById('author-input');
+  const yearInput = document.getElementById('year-input');
+  const pagesInput = document.getElementById('pages-input');
 
-dialog.addEventListener("close", () => {
-  const bookName = document.getElementById('book-name-input').value;
-  const author = document.getElementById('author-input').value;
-  const year = document.getElementById('year-input').value;
-  const pages = document.getElementById('pages-input').value;
+  const bookName = bookNameInput.value.trim();
+  const author = authorInput.value.trim();
+  const year = yearInput.value.trim();
+  const pages = pagesInput.value.trim();
   const isRead = document.getElementById('read-checkbox').checked;
-
-  dialogForm.reset();
 
   createdBook = addBookToLibrary(bookName, author, year, pages, isRead);
   addBookToContainer(createdBook);
+
+  dialog.close();
+
 });
 
 
-createdBook = addBookToLibrary("1984", "George Orwell", "1949", "200", true);
+dialog.addEventListener("close", () => {
+  dialogForm.reset();
+});
+
+
+createdBook = addBookToLibrary("1984", "George Orwell", "1949", "328", false);
 addBookToContainer(createdBook);
